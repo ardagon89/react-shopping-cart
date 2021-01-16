62ff0b32be8e3c8e4521b733d8d28174fcfb5c70
ardagon89 <shariq.mellon@gmail.com>
2021-01-16 14:18:31 -0600

    Added Counter component

--- app/react-app/src/components/counter.jsx
+++ app/react-app/src/components/counter.jsx
@@ -1,9 +1,29 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-    state = {  }
+    state = { 
+        count:0
+     };
+
+     styles = {
+         fontSize: 10,
+         fontWeight: "bold"
+     }
+
     render() { 
-        return ( <h1>Hello World!</h1>  );
+        return (
+          <React.Fragment>
+            <span style={this.styles} className="badge badge-primary m-2">
+              {this.formatCount()}
+            </span>
+            <button className="btn btn-secondary btn-sm">Increment</button>
+          </React.Fragment>
+        );
+    }
+
+    formatCount() {
+        const { count } = this.state;
+        return count === 0 ? 'Zero' : count;
     }
 }
  
